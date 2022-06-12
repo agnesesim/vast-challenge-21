3f1155574aa4ffeb65ef7cf7286c7718e93fb381
agnesesim <agnese.simonelli25@gmail.com>
2022-06-12 12:22:39 +0200

    bootstrap navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mt-0" id="app">
+      <PageHeader></PageHeader>
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import PageHeader from './components/PageHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    PageHeader
   }
 }
 </script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+<b-container class="bv-example-row">
+  <b-row>
+    <b-col>
+      <h1>prova pagina</h1>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+
+<script>
+
+
+
+export default {
+  name: 'HomePage',
+}
+</script>
