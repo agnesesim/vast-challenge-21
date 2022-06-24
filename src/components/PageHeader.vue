69a65d02d5143d7eef542c076a6812dae0dc2aae
agnesesim <agnese.simonelli25@gmail.com>
2022-06-24 17:47:35 +0200

    fix problem with hours plot

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">Visual Analytics Project</b-navbar-brand>
+    <b-navbar-brand>Visual Analytics Project</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Challenge 1</b-nav-item>
-        <b-nav-item href="#">Challenge 2</b-nav-item>
+        <b-nav-item style="point">Challenge 1</b-nav-item>
+        <b-nav-item>Challenge 2</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -18,8 +18,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.bg-info {
-    background-color: #0ac49f !important;
-}
-</style>
